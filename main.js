ef981c03609a31a18dc1e0c18f1fe0c841ee9fda
LAVEEZAFAREED <162338181+LAVEEZAFAREED@users.noreply.github.com>
2024-03-08 19:52:37 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+//Question 1:write addition,substraction,multiplication,and division operation that each result in  the number 8.
+//Enclose your operations in print statment.
+console.log(5 + 3); //Addition
+console.log(10 - 2); //Substraction
+console.log(4 * 2); //multiplication
+console.log(16 / 2); //division
+//Questiom 2:You should create four lines that look like this
+//console.log(5 + 3);
+//Your output should simply be four lines with tbe number 8 appearing once on each line.
+console.log(4 + 4); //Addition
+console.log(12 - 4); //Subsration
+console.log(2 * 4); //Multiplication
+console.log(24 / 3); //Division
+//Question 3:Store your favorite number in a variable.Then,create a message revealing your favorite number.
+//Print that message.
+var FavouriteNumber = 8; //This save ypur favourite number
+console.log("My Favourite Number is ".concat(FavouriteNumber, ".")); //This shows the message with your favourite number
